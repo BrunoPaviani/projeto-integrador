454ab03cc83a23549127966ba4b7f42e7ccd3e02
Bruno <brunopavi2010@gmail.com>
2019-10-08 08:10:56 -0300

    commit

--- src/components/solicitacao/ajusteDisciplina/AjusteDisciplina.js
+++ src/components/solicitacao/ajusteDisciplina/AjusteDisciplina.js
@@ -1,11 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { PickList } from 'primereact/picklist';
 
 export class AjusteDisciplina extends Component {
   constructor() {
     super();
+    this.state = {
+      picklistSourceCars: [],
+      picklistTargetCars: []
+    }
   }
 
   render() {
-    return (<div></div>);
+    return (<div className="p-col-12 p-lg-12">
+      <div className="card card-w-title">
+        <h1>Ajuste de Disciplina</h1>
+        <PickList source={this.state.picklistSourceCars} target={this.state.picklistTargetCars} sourceHeader="Disponíveis" targetHeader="Selecionadas"
+          responsive={true} itemTemplate={(car) => <span>{car.brand}</span>}
+          onChange={event => this.setState({ picklistSourceCars: event.source, picklistTargetCars: event.target })} />
+      </div>
+
+    </div>);
   }
 }
